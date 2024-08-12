fd08a39c141dc40f1e9f4a682599e1df719fc003
leostudiooo <lilingfeng0408@outlook.com>
2024-08-13 01:00:58 +0800

    Update cleaning structure

--- clean.py
+++ clean.py
@@ -2,18 +2,10 @@ import os
 import argparse
 import shutil
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Clean the dataset')
-    parser.add_argument('--original_dataset_path', type=str, default="datasets", help='Path to the original dataset')
-    parser.add_argument('--cleaned_dataset_path', type=str, default="cleaned", help='Path to the cleaned dataset')
-    args = parser.parse_args()
-
-    original_dataset_path = args.original_dataset_path
-    cleaned_dataset_path = args.cleaned_dataset_path
-
+def clean_dataset(original_dataset_path, cleaned_dataset_path):
     # 创建预处理前的数据集路径
     os.makedirs(original_dataset_path, exist_ok=True)
-	# 创建预处理后的数据集路径
+    # 创建预处理后的数据集路径
     os.makedirs(cleaned_dataset_path, exist_ok=True)
 
     # original_dataset_path/{experiment#}_{sweetness}/chu/{#}/{wav,jpg} -> cleaned_dataset_path/{sweetness}/重新编号/{*.wav,*.jpg}
@@ -25,7 +17,7 @@ if __name__ == '__main__':
     global_counter = 0
 
     for subdir in subdirs:
-        sweetness = subdir.split('_')[-1].split('\\')[0]
+        sweetness = subdir.split('_')[-1].split('/')[0]
 
         # go to subdir/chu/[#]
         folders = [f for f in os.listdir(os.path.join(original_dataset_path, subdir, 'chu')) if os.path.isdir(
@@ -41,3 +33,14 @@ if __name__ == '__main__':
             shutil.move(os.path.join(currPath, [f for f in os.listdir(currPath) if f.endswith('.jpg')][0]), os.path.join(destPath, str(global_counter) + '.jpg'))
             
             global_counter += 1
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Clean the dataset')
+    parser.add_argument('--original_dataset_path', type=str, default="datasets", help='Path to the original dataset')
+    parser.add_argument('--cleaned_dataset_path', type=str, default="cleaned", help='Path to the cleaned dataset')
+    args = parser.parse_args()
+
+    original_dataset_path = args.original_dataset_path
+    cleaned_dataset_path = args.cleaned_dataset_path
+
+    clean_dataset(original_dataset_path, cleaned_dataset_path)
